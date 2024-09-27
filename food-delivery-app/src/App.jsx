cdfba87af53e2efe30aea3a8c7cfb890a21c88db
RajithaKWG <rkrishan894@gmail.com>
2024-09-27 13:45:42 +0530

    Refactor App component to include Router and new pages

--- food-delivery-app/src/App.jsx
+++ food-delivery-app/src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
 
 function App() {
-
   return (
-    <div>
-      <Navbar />
-    </div>
-  )
+    <Router>
+      <div>
+      <h1>App Page</h1>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
