27e298e8d97d2edbab223d76a04800534d562f11
RajithaKWG <rkrishan894@gmail.com>
2024-09-27 17:53:18 +0530

    Refactor Navbar component and improve NavLink styling (Add Hover)

--- food-delivery-app/src/components/navbar/Navbar.jsx
+++ food-delivery-app/src/components/navbar/Navbar.jsx
@@ -1,52 +1,81 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { assets } from '../../assets/assets';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
   return (
     <div>
-        <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-            <img src={assets.logo} alt="" />
-            <ul className="flex space-x-4">
-                <li>
-                <NavLink 
-                    to="/" 
-                    className={({ isActive }) => isActive ? "text-yellow-500" : "text-white hover:text-yellow-300"} 
-                    exact
-                >
-                    Home
-                </NavLink>
-                </li>
-                <li>
-                <NavLink 
-                    to="/menu" 
-                    className={({ isActive }) => isActive ? "text-yellow-500" : "text-white hover:text-yellow-300"}
-                >
-                    Menu
-                </NavLink>
-                </li>
-                <li>
-                <NavLink 
-                    to="/about" 
-                    className={({ isActive }) => isActive ? "text-yellow-500" : "text-white hover:text-yellow-300 "}
-                >
-                    About
-                </NavLink>
-                </li>
-                <li>
-                <NavLink 
-                    to="/contact" 
-                    className={({ isActive }) => isActive ? "text-yellow-500" : "text-white hover:text-yellow-300"}
-                >
-                    Contact
-                </NavLink>
-                </li>
-            </ul>
-            </div>
-        </nav>
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <img src={assets.logo} alt="" />
+          <ul className="flex space-x-4">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-yellow-500"
+                    : "text-white hover:text-yellow-300"
+                }
+                exact
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/menu"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-yellow-500"
+                    : "text-white hover:text-yellow-300"
+                }
+              >
+                Menu
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-yellow-500"
+                    : "text-white hover:text-yellow-300 "
+                }
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-yellow-500"
+                    : "text-white hover:text-yellow-300"
+                }
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+          <div className="flex justify-between items-center space-x-4">
+            <Link to="/search">
+              <img src={assets.search_icon} alt="" />
+            </Link>
+            <Link to="/cart">
+              <img src={assets.basket_icon} alt="" />
+            </Link>
+            <NavLink to="/login">
+              <button className="bg-yellow-500 text-white hover:bg-black px-4 py-2 rounded-full">
+                Sign in
+              </button>
+            </NavLink>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
