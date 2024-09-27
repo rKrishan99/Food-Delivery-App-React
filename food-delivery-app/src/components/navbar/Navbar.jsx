93d245c157c7ffa8bd272ae97a4885a1ac20a292
RajithaKWG <rkrishan894@gmail.com>
2024-09-27 17:27:50 +0530

    Refactor Navbar component and improve NavLink styling (Add Hover)

--- food-delivery-app/src/components/navbar/Navbar.jsx
+++ food-delivery-app/src/components/navbar/Navbar.jsx
@@ -1,60 +1,52 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { assets } from "../../assets/assets";
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../../assets/assets';
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="bg-gray-800 p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <img src={assets.logo} alt="" />
-          <ul className="flex space-x-4">
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive ? "text-yellow-500" : "text-white"
-                }
-                exact
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/menu"
-                className={({ isActive }) =>
-                  isActive ? "text-yellow-500" : "text-white"
-                }
-              >
-                Menu
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about"
-                className={({ isActive }) =>
-                  isActive ? "text-yellow-500" : "text-white"
-                }
-              >
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/contact"
-                className={({ isActive }) =>
-                  isActive ? "text-yellow-500" : "text-white"
-                }
-              >
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        <nav className="bg-gray-800 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+            <img src={assets.logo} alt="" />
+            <ul className="flex space-x-4">
+                <li>
+                <NavLink 
+                    to="/" 
+                    className={({ isActive }) => isActive ? "text-yellow-500" : "text-white hover:text-yellow-300"} 
+                    exact
+                >
+                    Home
+                </NavLink>
+                </li>
+                <li>
+                <NavLink 
+                    to="/menu" 
+                    className={({ isActive }) => isActive ? "text-yellow-500" : "text-white hover:text-yellow-300"}
+                >
+                    Menu
+                </NavLink>
+                </li>
+                <li>
+                <NavLink 
+                    to="/about" 
+                    className={({ isActive }) => isActive ? "text-yellow-500" : "text-white hover:text-yellow-300 "}
+                >
+                    About
+                </NavLink>
+                </li>
+                <li>
+                <NavLink 
+                    to="/contact" 
+                    className={({ isActive }) => isActive ? "text-yellow-500" : "text-white hover:text-yellow-300"}
+                >
+                    Contact
+                </NavLink>
+                </li>
+            </ul>
+            </div>
+        </nav>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
