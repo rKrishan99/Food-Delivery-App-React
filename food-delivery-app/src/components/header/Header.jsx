21f2bfb819dd39783a1733e51134970132a69d33
RajithaKWG <rkrishan894@gmail.com>
2024-09-28 12:53:16 +0530

    Refactor Header component and improve styling

--- food-delivery-app/src/components/header/Header.jsx
+++ food-delivery-app/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div>
       <div className="bg-hero-img bg-cover rounded-3xl h-500px  p-15 ml-app-margin-x mr-app-margin-x relative">
-        <div className="absolute flex pl-20 flex-col items-start gap-6 max-w-2xl bottom-0 left-0 p-8">
+        <div className="absolute flex pl-20 flex-col items-start gap-6 max-w-2xl bottom-0 left-0 p-8 ">
           <h1 className="text-5xl font-medium text-white max-w-lg leading-[4rem]">Order your favourite food here</h1>
           <p className="text-white">
             Choose from a divers menu featuring a delectable array of dishes
