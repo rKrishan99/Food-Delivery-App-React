f3cacc63f805947b5e77eb19d6304b2cd370bbe1
RajithaKWG <rkrishan894@gmail.com>
2024-09-30 23:22:26 +0530

    Refactor FoodItem component to use cartItems from StoreContext

--- food-delivery-app/src/components/foodItem/FoodItem.jsx
+++ food-delivery-app/src/components/foodItem/FoodItem.jsx
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
-import { useState } from "react";
+import { StoreContext } from "../../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-
-    const [itemCount, setItemCount] = useState(0);
+  
+  const [cartItems, addToCart, removeFormCart] = useContext(StoreContext);
 
   return (
     <div>
       <div className="w-full shadow-md rounded-md m-auto transition duration-300 overflow-hidden">
         <div className="relative">
-        <img
-          src={image}
-          alt=""
-          className=" w-full rounded-tl-md rounded-tr-md rounded-bl-none rounded-br-none"
-        />
-        {!itemCount
-            ? <img src={assets.add_icon_white} 
-            className="w-8 h-8 absolute right-2 bottom-2"
-            onClick={() => setItemCount(prev=>prev+1)} 
-            alt=""/>
-            : <div className="absolute right-2 bottom-2 flex items-center gap-3 p-1 rounded-full bg-white "> 
-                <img className="w-8 h-8" onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
-                <p>{itemCount}</p>
-                <img className="w-8 h-8" onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
-
+          <img
+            src={image}
+            alt=""
+            className=" w-full rounded-tl-md rounded-tr-md rounded-bl-none rounded-br-none"
+          />
+          {!cartItems[id] ? (
+            <img
+              src={assets.add_icon_white}
+              className="w-8 h-8 absolute right-2 bottom-2"
+              onClick={() => addToCart(id)}
+              alt=""
+            />
+          ) : (
+            <div className="absolute right-2 bottom-2 flex items-center gap-3 p-1 rounded-full bg-white ">
+              <img
+                className="w-8 h-8"
+                onClick={() => removeFormCart(id)}
+                src={assets.remove_icon_red}
+                alt=""
+              />
+              <p>{cartItems[id]}</p>
+              <img
+                className="w-8 h-8"
+                onClick={() => addToCart(id)}
+                src={assets.add_icon_green}
+                alt=""
+              />
             </div>
-        }
+          )}
         </div>
         <div className="p-6 ">
           <div className="flex justify-between items-center	mb-3">
--- food-delivery-app/src/context/StoreContext.jsx
+++ food-delivery-app/src/context/StoreContext.jsx
@@ -1,18 +1,40 @@
-import { createContext } from 'react';
-import { food_list } from '../assets/assets';
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  const removeFormCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }), [cartItems];
+
   const contextValue = {
-    food_list: food_list,
-  }
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFormCart,
+  };
 
   return (
     <StoreContext.Provider value={contextValue}>
-      {props.children}  
+      {props.children}
     </StoreContext.Provider>
   );
-}
+};
 
 export default StoreContextProvider;
