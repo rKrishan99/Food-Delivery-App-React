d994355f723a7da1ac1224416dd80b0b239180c8
RajithaKWG <rkrishan894@gmail.com>
2024-09-30 11:48:12 +0530

    Refactor Home component and add category state to ExploreMenu

--- food-delivery-app/src/pages/home/Home.jsx
+++ food-delivery-app/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/header/Header'
 import ExploreMenu from '../../components/exploreMenu/ExploreMenu'
+import { useState } from 'react'
 
 const Home = () => {
+  const [category, setCategory] = useState('All')
   return (
     <div>
       <Header/>
-      <ExploreMenu/>
+      <ExploreMenu  />
     </div>
   )
 }
